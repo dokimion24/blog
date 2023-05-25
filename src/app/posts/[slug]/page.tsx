7833af8cd3cd1230ea969ffc22fca251b98f7014
dokimion24 <leecy3610@gmail.com>
2023-05-25 22:32:49 +0900

    design: post detail 페이지 css 스타일

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -1,6 +1,6 @@
 import { getPostData } from '@/api/posts'
 import MarkdownViewer from '@/components/Markdown/MarkdownViewer'
-import React from 'react'
+import Image from 'next/image'
 
 interface Props {
   params: {
@@ -12,9 +12,21 @@ export default async function PostDetailPage({ params }: Props) {
   const post = await getPostData(params.slug)
 
   return (
-    <>
-      <h1>{post.title}</h1>
-      <MarkdownViewer content={post.content} />
-    </>
+    <article className="rounded-2xl overflow-hidden bg-gray-100 shadow-lg m-4">
+      <Image
+        className="w-full h-1/5"
+        src={`/images/posts/${post.path}.png`}
+        alt={post.title}
+        width={760}
+        height={420}
+      />
+      <section className="flex flex-col p-4">
+        <p className="font-semibold ml-2">{post.date.toString()}</p>
+        <h1 className="text-4xl font-bold">{post.title}</h1>
+        <p className="text-xl font-bold">{post.description}</p>
+        <div className="w-44 border-2 border-sky-600 mt-4 mb-8"></div>
+        <MarkdownViewer content={post.content} />
+      </section>
+    </article>
   )
 }
